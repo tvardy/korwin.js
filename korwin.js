ea833545e1ece92947cd84d6dec6f5ce93b9f527
Grzegorz Twardowski <tvardy@users.noreply.github.com>
2015-11-23 15:30:41 +0100

    Update korwin.js
    - small update of code styling
    - enclosed everything in SIAF (Self-Invocing Anonymous Function)
    - added 'use strict'

--- korwin.js
+++ korwin.js
@@ -1,7 +1,9 @@
-[].filter.call(
-	document.body.getElementsByTagName('*'),
-	function(el) {
-		return getComputedStyle(el).float !== 'left';
-	}).forEach(function (el) {
-		el.style.float = 'right';
-	})
+(function() {
+  'use strict';
+  
+  [].filter.call(document.body.getElementsByTagName('*'), function(el) {
+    return getComputedStyle(el).float !== 'left';
+  }).forEach(function (el) {
+    el.style.float = 'right';
+  });
+})();
