1e5bfadf96ce806bbe1ca043680c210c7d6ff09d
Grzegorz (Greg) Twardowski <tvardy@users.noreply.github.com>
2022-07-28 20:59:54 +0000

    modernise syntax

--- korwin.js
+++ korwin.js
@@ -1,4 +1,5 @@
-(function() {
+// version 2017
+(function(root) {
   'use strict';
   
   var change = {
@@ -10,7 +11,7 @@
   [].filter.call(getAllElements(), isLookedFor).forEach(applyChange);
   
   function getAllElements () {
-    return document.body.getElementsByTagName('*');
+    return root.getElementsByTagName('*');
   }
   
   function isLookedFor (el) {
@@ -20,4 +21,21 @@
   function applyChange (el) {
     el.style[change.what] = change.to;
   }
-})();
+})(document.body);
+
+// version 2022
+((root) => {
+  const change = {
+    what: 'float',
+    from: 'left',
+    to: 'right'
+  }
+
+  const API = {
+    getAllElements: () => root.getElementsByTagName('*'),
+    isLookedFor: el => getComputedStyle(el)[change.what] === change.from,
+    applyChange: el => el.style[change.what] = change.to
+  }
+
+  API.getAllElements().filter(API.isLookedFor).forEach(applyChange)
+})(document.body)
